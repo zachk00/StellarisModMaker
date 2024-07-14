7f3e3376bf1c0b9ae4c8b8b41e47d464154109e4
Zach-Dominion <zachkerr00@gmail.com>
2024-07-13 22:20:29 -0400

    remove comment
    testing rulset

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Create database tables if they don't exist
+        db.create_all() 
     app.run(debug=True)
