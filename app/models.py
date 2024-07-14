f6f99f0d394ff646fd51d1257102d05973346f6e
Zach-Dominion <zachkerr00@gmail.com>
2024-07-14 00:39:02 -0400

    Update models.py
    added mod tables

--- app/models.py
+++ app/models.py
@@ -22,3 +22,18 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class ModType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False, unique=True)
+    description = db.Column(db.String(255), nullable=True)
+
+class Mod(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    status = db.Column(db.String(50), nullable=False, default='In Progress')
+    mod_type_id = db.Column(db.Integer, db.ForeignKey('mod_type.id'), nullable=False)
+    mod_type = db.relationship('ModType', backref=db.backref('mods', lazy=True))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
