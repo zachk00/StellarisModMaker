d74b924e963d6a5cde26f406c379647365ea2ac9
Zach-Dominion <zachkerr00@gmail.com>
2024-07-14 00:51:06 -0400

    basic dashboard setup

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from flask_login import login_required
 from . import main
+from app.models import Mod, ModType
+from flask_login import login_required, current_user
 
 @main.route('/')
 def index():
@@ -9,4 +11,18 @@ def index():
 @main.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    mod_types = ModType.query.all()
+    user_mods = Mod.query.filter_by(user_id=current_user.id).all()
+    return render_template('dashboard.html', user_mods=user_mods, mod_types=mod_types)
+
+@main.route('/mod_editor/<mod_type>')
+@login_required
+def mod_editor(mod_type):
+    mod_type = ModType.query.filter_by(name=mod_type).first_or_404()
+    return render_template('mod_editor.html', mod_type=mod_type)
+
+@main.route('/mod_editor/<int:mod_id>')
+@login_required
+def edit_mod(mod_id):
+    mod = Mod.query.get_or_404(mod_id)
+    return render_template('mod_editor.html', mod=mod, mod_type=mod.mod_type)
